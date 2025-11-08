b6f2259178d41b17dbee9d99658c999dfbebab74
devloperops2-max <devloperops2@gmail.com>
2025-11-08 13:34:21 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,7 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: "AIzaSyAZkaiUn7WP49FEqzJxGKwaI8480NJCYk8"})],
   model: 'googleai/gemini-2.5-flash',
 });
+
